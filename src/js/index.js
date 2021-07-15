543647a3c3c016ebc5e7c3e73dbe722c41cabcdc
Batzaya0112 <batzaya@Batzaya-3.local>
2021-07-15 22:41:29 +0800

    docs folder uusgelee. gitpage ashiglahiin tuld

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,4 @@
-import query from "./Model/Search";
-import { add, multiply as m } from "./View/searchView";
-console.log("хайлт: " + query);
-console.log("add " + add(6, 2));
-console.log(m(6, 2));
+require("@babel/polyfill");
+import Search from "./model/Search";
+let search = new Search('pasta');
+search.doSearch().then(r => console.log(r));
