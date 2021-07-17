bc4be7c5a8e344b9181d2d80cb9eca8c3db4e833
Batzaya0112 <batzaya8971@gmail.com>
2021-07-17 18:34:22 +0800

    searchiin icon gargana

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,6 @@
 require("@babel/polyfill");
 import Search from "./model/Search";
-import {elements} from "./View/base";
+import {elements, renderLoader, clearLoader} from "./View/base";
 import * as searchView from "./View/searchView";
  /**
   * web app төлөв
@@ -21,10 +21,12 @@ const controlSearch = async () => {
         //3. Хайлт хийхэд зориулж дэлгэцийг UI бэлтгэнэ.
         searchView.clearSearchQuery();
         searchView.clearSearchResult();
+        renderLoader(elements.searchResultDiv);
 
         //4. Хайлтыг гүйцэтгэнэ.
         await state.search.doSearch();
         //5. Хайлтын үр дүнг дэлгэцэнд үзүүлнэ.
+        clearLoader();
         if(state.search.result === undefined) alert('Хайлт илэрцгүй...');
         else searchView.renderRecipes(state.search.result);
     }
